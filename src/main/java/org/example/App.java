8b645ccf1dcfec10e675e75927b1c169efb5ebb9
Javad <javadnazirli24@gmail.com>
2025-12-05 19:39:19 +0400

    fix(repo): All repository implement classes

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -1,5 +1,11 @@
 package org.example;
 
+import org.example.config.DatabaseConfig;
+import org.example.enums.*;
+import org.example.exception.NotFoundException;
+import org.example.model.Car;
+import org.example.repository.car.CarRepositoryImpl;
+
 /**
  * Hello world!
  *
@@ -8,6 +14,17 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        Car testCar = Car.builder()
+                .brand(CarBrand.BMW) // BMW
+                .model(CarModel.SERIES_3) // SERIES_3
+                .year(2020)
+                .transmissionType(TransmissionType.AUTOMATIC) // AUTOMATIC
+                .fuelType(FuelType.PETROL) // PETROL
+                .color("Black")
+                .kilometersDriven(50000.0)
+                .price(35000.0)
+                .costPrice(30000.0)
+                .saleStatus(SaleStatus.IN_STOCK) // IN_STOCK
+                .build();
     }
 }
